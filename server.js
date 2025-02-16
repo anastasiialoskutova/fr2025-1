53f5a15e4c117b90547732709dc62f675d847acf
Anastasiia Loskutova <anastasiialoskutova11@gmail.com>
2025-02-16 20:31:17 +0200

    Empty commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('uploads'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const storage = multer.diskStorage({
+const storage = multer.diskStorage({//
     destination: 'uploads/',
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
